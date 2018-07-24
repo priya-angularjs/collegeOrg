a5a40f16e699081e9e5d26f947a2b65726f5e301
Priya Murugan <priyadharshini.m@veetechnologies.com>
2018-07-24 17:03:21 +0530

    add changes in login component with social login module
    Signed-off-by: Priya Murugan <priyadharshini.m@veetechnologies.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,32 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import {PanelMenuModule} from 'primeng/panelmenu';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  LinkedinLoginProvider
+} from 'angular-6-social-login';
+
+export function getAuthServiceConfigs() {
+  const config = new AuthServiceConfig(
+    [
+      {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider('Your-Facebook-app-id')
+      },
+      {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider('Your-Google-Client-Id')
+      },
+      {
+        id: LinkedinLoginProvider.PROVIDER_ID,
+        provider: new LinkedinLoginProvider('1098828800522-m2ig6bieilc3tpqvmlcpdvrpvn86q4ks.apps.googleusercontent.com')
+      },
+    ]);
+  return config;
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,9 +67,14 @@ import {PanelMenuModule} from 'primeng/panelmenu';
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
-    PanelMenuModule
+    PanelMenuModule,
+    SocialLoginModule
   ],
-  providers: [{provide: APP_CONFIG, useValue: AppConfig}],
+  providers: [{provide: APP_CONFIG, useValue: AppConfig},
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
-
+import {
+  AuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  LinkedinLoginProvider
+} from 'angular-6-social-login';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +15,8 @@ import {Router} from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private route: Router) {
+  constructor(private fb: FormBuilder, private route: Router,
+              private socialAuthService: AuthService) {
   }
 
   ngOnInit() {
@@ -28,5 +34,24 @@ export class LoginComponent implements OnInit {
       alert('Login Failed.Enter correct password');
     }
 
+  }
+  public socialSignIn(socialPlatform: string) {
+    let socialPlatformProvider;
+    if (socialPlatform === 'facebook') {
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    } else if (socialPlatform === 'google') {
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    } else if (socialPlatform === 'linkedin') {
+      socialPlatformProvider = LinkedinLoginProvider.PROVIDER_ID;
+    }
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform + 'sign in data : ' , userData);
+        // Now sign-in with userData
+        // ...
+
+      }
+    );
   }
 }
